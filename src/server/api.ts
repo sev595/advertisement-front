72ad9b280da3aeba4a3e7ed235d075d2ddfd30e3
Sevak <sevak@Sevaks-MacBook-Pro.local>
2024-12-17 14:34:56 +0400

    fixed fetch

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,19 +1,11 @@
 export const fetchData = async (endpoint: string) => {
-    const queryString = `${import.meta.env.VITE_API_URL}api/${endpoint}`;
+    const queryString = `${import.meta.env.VITE_API_URL}api/${endpoint}`
     const response = await fetch(queryString, {
         method: 'GET',
         headers: {
-            'Authorization': `Bearer ${import.meta.env.VITE_BARIER_TOKEN}`, // Bearer token for authentication
-            'Content-Type': 'application/json', // Ensures the server understands JSON
-            // 'Accept': 'application/json', // Specifies the response format expected
-            // 'X-Requested-With': 'XMLHttpRequest' // Sometimes required for CSRF protection
+            'Authorization': `Bearer ${import.meta.env.VITE_BARIER_TOKEN}`, // Add your Bearer token here
+            'Content-Type': 'application/json'
         }
     });
-
-    // Handle errors or unexpected response
-    if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    return response.json(); // Parse the response as JSON
-};
+    return response
+}
