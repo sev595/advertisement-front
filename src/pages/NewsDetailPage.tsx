aa0fd1e4a68e2951a60624347c5cd9de620f0154
Sevak <sevak@Sevaks-MacBook-Pro.local>
2025-01-24 22:46:10 +0400

    test

--- src/pages/NewsDetailPage.tsx
+++ src/pages/NewsDetailPage.tsx
@@ -78,7 +78,7 @@ export default function NewsDetailPage() {
         </div>
         <div data-type="_mgwidget" data-widget-id="1716515">
         </div>
-
+qqq
 <script async="async" data-cfasync="false" src="//pl25668362.profitablecpmrate.com/894e73c9cfdd621e54ce14132c876c9a/invoke.js"></script>
 <div id="container-894e73c9cfdd621e54ce14132c876c9a"></div>
         {/* {(function (w, q) { w[q] = w[q] || []; w[q].push(["_mgc.load"]) })(window, "_mgq")} */}
